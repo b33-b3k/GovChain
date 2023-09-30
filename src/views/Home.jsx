e518daa5df993ae64818c346e473c1a6819987c3
Santosh Yadav <129085397+santoshyadav-29@users.noreply.github.com>
2023-09-30 23:57:58 +0545

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,19 @@
-import Banner from '../components/Banner'
-import CreateProposal from '../components/CreateProposal'
-import Proposals from '../components/Proposals'
+import Banner from "../components/Banner";
+import CreateProposal from "../components/CreateProposal";
+import Proposals from "../components/Proposals";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
+      <Header />
       <Banner />
       <Proposals />
       <CreateProposal />
+      <Footer/>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
